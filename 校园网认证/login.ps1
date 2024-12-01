e163185fe297c352447aaf75b6b4506fc4554806
Howl-24 <Howell.Ding@outlook.com>
2024-12-01 15:25:49 +0800

    add feature
    增加无线网络登录未找到网络信息的报错

--- 校园网认证/login.ps1
+++ 校园网认证/login.ps1
@@ -1,7 +1,7 @@
 $account = ""          # 在此填写学号
 $password = ""         # 在此填写密码
 $operator = ""         # 在此填写运营商（校园网为空，移动:@cmcc，联通:@unicom，电信:@telecom）
-$wireless = [bool]$    # 填写网络连接方式，无线为 true，有线为 false
+$wireless =      # 填写网络连接方式，无线为 $true，有线为 $false
 
 if($wireless)
 {
@@ -14,31 +14,29 @@ if($wireless)
         $_.InterfaceAlias -like "*Wi-Fi*" -or $_.InterfaceAlias -like "*WLAN*" -and $_.IPv4Address -ne $null 
     }
 
-    # 提取并格式化 Wi-Fi 的 IP 和 MAC 地址
-    $wifiInfo | ForEach-Object {
-        $ip = $_.IPv4Address.IPAddress
-        $mac = ($_.NetAdapter.MacAddress -replace "-", "").ToLower()
-        # Write-Output "$ip"
-        # Write-Output "$mac"
+    if ($wifiInfo) {
+        # 选择第一个匹配的网络接口
+        $firstWiFi = $wifiInfo | Select-Object -First 1
+        $ip = $firstWiFi.IPv4Address.IPAddress
+        $mac = ($firstWiFi.NetAdapter.MacAddress -replace "-", "").ToLower()
+
+        # 构造 URL
+        $url = "http://10.0.1.5:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=,0,$account$operator&user_password=$passwordBase64&wlan_user_ip=$ip&wlan_user_ipv6=&wlan_user_mac=$mac&wlan_ac_ip=10.32.255.10&wlan_ac_name=HJ-BRAS-ME60-01&jsVersion=4.2&terminal_type=1&lang=zh&lang=zh&v=8405"
+        Write-Output "$url"
+
+        # 发出 HTTP 请求
+        Invoke-WebRequest -Uri $url
     }
+    else { Write-Error "未找到有效的网络信息"}
 
-    # 构造 URL
-    $url = "http://10.0.1.5:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=,0,$account$operator&user_password=$passwordBase64&wlan_user_ip=$ip&wlan_user_ipv6=&wlan_user_mac=$mac&wlan_ac_ip=10.32.255.10&wlan_ac_name=HJ-BRAS-ME60-01&jsVersion=4.2&terminal_type=1&lang=zh&lang=zh&v=8405"
-    # Write-Output "$url"
-
-    # 发出 HTTP 请求
-    Invoke-WebRequest -Uri $url
-
-    return
+    
 }  
 else
 {
     # 构造 URL
     $url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=$account$operator&upass=$password"
-    # Write-Output "$url"
+    Write-Output "$url"
 
     # 发出 HTTP 请求
     Invoke-WebRequest -Uri $url
-
-    return
 }
